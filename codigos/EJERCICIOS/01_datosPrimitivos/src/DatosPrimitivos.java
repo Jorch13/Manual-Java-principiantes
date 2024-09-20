21f1cbd480b58a3844ebeeb49e6b149c56950446
jorch13 <jorgemd1308@gmail.com>
2024-09-20 13:06:24 +0200

    Se crea el directorio para los ejercicios.

---
+++ codigos/EJERCICIOS/01_datosPrimitivos/src/DatosPrimitivos.java
@@ -0,0 +1,20 @@
+public class DatosPrimitivos {
+    public static void main(String[] args) throws Exception {
+        // Declara las variables
+
+        
+        /*
+         * Suma las variables
+         * (puedes usar una variable extra como contador de goles)
+         */
+
+
+
+        // Muestra el resultado de la suma total de los goles
+        System.out.println("--------------------------------");
+        System.out.println("Goles totales en los ultimos partidos: ");
+
+        System.out.println("--------------------------------");
+
+    }
+}
